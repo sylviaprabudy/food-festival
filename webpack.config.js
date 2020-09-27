19579aa06f81e799ec9292dfd313f0d6b3251562
Sylvia <sylviaprabudy@gmail.com>
2020-09-27 00:13:37 -0500

    convert app to a PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,64 @@
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const path = require('path');
 const webpack = require("webpack");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 module.exports = {
-  entry: {
-    app: "./assets/js/script.js",
-    events: "./assets/js/events.js",
-    schedule: "./assets/js/schedule.js",
-    tickets: "./assets/js/tickets.js"
-  },  
-  output: {
-    filename: "[name].bundle.js",
-    path: __dirname + "/dist"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jpg$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name(file) {
-                return '[path][name].[ext]';
-              },
-              publicPath: function(url) {
-                return url.replace('../', '/assets/');
-              }
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: __dirname + "/dist"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name(file) {
+                                return '[path][name].[ext]';
+                            },
+                            publicPath: function (url) {
+                                return url.replace('../', '/assets/');
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
             }
-          },
-          {
-            loader: 'image-webpack-loader'
-          }
         ]
-      }
-    ]
-  },
-  plugins:[
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
-    }),
-    new BundleAnalyzerPlugin({
-      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
-    })
-  ],
-  mode: 'development'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        }),
+        new WebpackPwaManifest({
+            name: "Food Event",
+            short_name: "Foodies",
+            description: "An app that allows you to view upcoming food events.",
+            background_color: "#01579b",
+            theme_color: "#ffffff",
+            fingerprints: false,
+            inject: false,
+            icons: [{
+              src: path.resolve("assets/img/icons/icon-512x512.png"),
+              sizes: [96, 128, 192, 256, 384, 512],
+              destination: path.join("assets", "icons")
+            }]
+          })
+    ],
+    mode: 'development'
 };
